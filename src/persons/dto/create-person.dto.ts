8b25c66c5d2431424770fc9260e027986f65c4b5
Daniel Silva <djs@cin.ufpe.br>
2023-01-10 16:08:12 -0300

    initial SWAGGER documentation

--- src/persons/dto/create-person.dto.ts
+++ src/persons/dto/create-person.dto.ts
@@ -1,8 +1,21 @@
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
+
 export class CreatePersonDto {
+  @ApiProperty({ example: 'Jane Doe' })
   name: string;
+
+  @ApiProperty({ example: '012.345.678-90' })
   cpf: string;
+
+  @ApiProperty({ example: 'jdoe@gmail.com' })
   email: string;
+
+  @ApiPropertyOptional({ example: 'Female' })
   gender?: string;
+
+  @ApiPropertyOptional({ example: 27 })
   age?: number;
+
+  @ApiPropertyOptional({ example: '2023-01-10T19:00:39.035Z' })
   created_at?: Date;
 }
--- src/persons/persons.controller.ts
+++ src/persons/persons.controller.ts
@@ -7,35 +7,52 @@ import {
   Param,
   Delete,
 } from '@nestjs/common';
+import {
+  ApiBearerAuth,
+  ApiOperation,
+  ApiResponse,
+  ApiTags,
+} from '@nestjs/swagger';
 import { PersonsService } from './persons.service';
 import { CreatePersonDto } from './dto/create-person.dto';
 import { UpdatePersonDto } from './dto/update-person.dto';
 
+@ApiBearerAuth()
+@ApiTags('Persons')
 @Controller('persons')
 export class PersonsController {
   constructor(private readonly personsService: PersonsService) {}
 
   @Post()
+  @ApiOperation({ summary: 'Create a person' })
+  @ApiResponse({
+    status: 201,
+    description: 'The record has been successfully created.',
+  })
   create(@Body() createPersonDto: CreatePersonDto) {
     return this.personsService.create(createPersonDto);
   }
 
   @Get()
+  @ApiOperation({ summary: 'List all persons' })
   findAll() {
     return this.personsService.findAll();
   }
 
   @Get(':id')
+  @ApiOperation({ summary: 'Find one person' })
   findOne(@Param('id') id: string) {
     return this.personsService.findOne(id);
   }
 
   @Patch(':id')
+  @ApiOperation({ summary: 'Update one person' })
   update(@Param('id') id: string, @Body() updatePersonDto: UpdatePersonDto) {
     return this.personsService.update(id, updatePersonDto);
   }
 
   @Delete(':id')
+  @ApiOperation({ summary: 'Delete one person' })
   remove(@Param('id') id: string) {
     return this.personsService.remove(id);
   }
